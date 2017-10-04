7600b60fe9dc8a5bded5519463722e1ce6f4a8e9
mrkjdvs <mjd814@gmail.com>
2017-10-04 13:30:39 +0100

    All tests passing for raindrops.

--- ecmascript/raindrops/raindrops.js
+++ ecmascript/raindrops/raindrops.js
@@ -2,9 +2,9 @@ export default class Raindrops {
   convert(number) {
     this.number = number;
     this.result = '';
-    if (this.number % 3 === 0) this.result =+ 'Pling';
-    if (this.number % 5 === 0) this.result =+ 'Plang';
-    if (this.number % 3 === 0) this.result =+ 'Plong';
+    if (this.number % 3 === 0) this.result = this.result + 'Pling';
+    if (this.number % 5 === 0) this.result = this.result + 'Plang';
+    if (this.number % 7 === 0) this.result = this.result + 'Plong';
     return this.result ? this.result : this.number.toString();
   }
 }
