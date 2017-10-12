08b9e6ab14b904d5f7bde682e18d07943125d165
mrkjdvs <mjd814@gmail.com>
2017-10-12 12:21:06 +0100

    decoded method defined for simple cipher and test passed

--- ecmascript/simple-cipher/simple-cipher.js
+++ ecmascript/simple-cipher/simple-cipher.js
@@ -1,6 +1,7 @@
 export default class Cipher {
   constructor() {
-    this.key = 'dddddddddd';
+    this.key = 'dddddddddd'; // define a key
+    // could set lines 11 and 20 to the difference between this key and its place in the alphabet
     this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
   }
 
@@ -8,8 +9,19 @@ export default class Cipher {
     const encodedString = [];
     for (let i = 0; i < string.length; i++) {
       const index = this.alphabet.indexOf(string[i]);
+      // grab the index of each letter of the string to encode in the alphabet
       encodedString[i] = this.alphabet[index + 3];
+      // apply the Caesar cipher i.e. difference of three as stated in readme
     }
     return encodedString.join('');
+  }
+
+  decode(encodedString) {
+    const decodedString = [];
+    for (let i = 0; i < encodedString.length; i++) {
+      const index = this.alphabet.indexOf(encodedString[i]);
+      decodedString[i] = this.alphabet[index - 3];
+    }
+    return decodedString.join('');
   }
 }
